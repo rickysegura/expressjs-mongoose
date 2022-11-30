1f083b36b661f7e9be352261e511d26de46fc65c
Rico The Dev <ricothedev@gmail.com>
2022-11-30 02:01:22 -0800

    FIRST TEST
    Rewriting initial response from server.

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
+  res.json({ message: "Testing to see if this works." });
 });
 
 app.use("/countries", countryRoutes);
